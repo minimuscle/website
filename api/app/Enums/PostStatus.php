90f2d640f5ccd6696a5680b3cf813ca061e76aee
minimuscle <joshthiele@live.com.au>
2026-08-15 23:33:00 +0000

    feat: implement basic laravel backend to retrieve posts

---
+++ api/app/Enums/PostStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum PostStatus
+{
+    case DRAFT;
+    case PUBLISHED;
+    case PUBLISHED_REVIEW;
+
+    
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,5 @@ Route::get('/health', function () {
         'status' => 'ok',
     ];
 });
+
+Route::get('posts', [PostController::class, 'index']);
